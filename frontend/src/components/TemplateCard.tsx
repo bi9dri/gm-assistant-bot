b77ae41b942fc0ff7cf29078c155ac7466242ae3
bi9dri <zero.256dic+bi9di.github.com@gmail.com>
2025-12-25 01:18:16 +0900

    feat: Refactor TemplateCard to remove guild information and update last updated display

--- frontend/src/components/TemplateCard.tsx
+++ frontend/src/components/TemplateCard.tsx
@@ -4,26 +4,18 @@ import z from "zod";
 export const TemplateCardSchema = z.object({
   id: z.int(),
   name: z.string().trim().nonempty(),
-  guild: z.object({
-    id: z.string().trim().nonempty(),
-    name: z.string().trim().nonempty(),
-    icon: z.url(),
-  }),
   updatedAt: z.date(),
 });
 
 type Props = z.infer<typeof TemplateCardSchema>;
 
-export const TemplateCard = ({ id, name, guild, updatedAt }: Props) => {
+export const TemplateCard = ({ id, name, updatedAt }: Props) => {
   return (
     <div className="card">
       <div className="card-body">
         <h5 className="card-title">{name}</h5>
-        <h6 className="card-subtitle mb-2 text-muted">
-          {guild.name} (ID: {guild.id})
-        </h6>
         <p className="card-text">
-          Last Updated: {updatedAt.toLocaleDateString()} {updatedAt.toLocaleTimeString()}
+          Last Updated: {updatedAt.toLocaleString()}
         </p>
         <div className="card-actions">
           <Link to="/template/$id" params={{ id: id.toString() }} className="btn btn-primary">
--- frontend/src/routes/template/index.tsx
+++ frontend/src/routes/template/index.tsx
@@ -1,16 +1,27 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
+import { useLiveQuery } from "dexie-react-hooks";
+
+import { TemplateCard } from "@/components/TemplateCard";
+import { db } from "@/db";
 
 export const Route = createFileRoute("/template/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const templates = useLiveQuery(() => db.Template.toArray());
+
   return (
     <>
       <h1 className="text-3xl inline-block">テンプレート</h1>
       <Link to="/template/new" className="btn btn-primary ml-8 mb-4">
         新しいテンプレートを作成
       </Link>
+      <div className="flex flex-wrap gap-8">
+        {templates?.map((t) => (
+          <TemplateCard key={t.id} id={t.id} name={t.name} updatedAt={t.updatedAt} />
+        ))}
+      </div>
     </>
   );
 }
